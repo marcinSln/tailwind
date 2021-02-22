1840a42c1fa5bfd38a4dff525553bb51317db60c
Marcin Smoleń <marcinememo@gmail.com>
2021-02-22 23:21:58 +0100

    Complete Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,46 +1,73 @@
 import backgroundImage from '../img/background.png';
 import { Link } from 'react-router-dom';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
-export const Header = () => {
-	return (
-		<div className="container mx-20 flex">
-			<div className="flex-1 flex">
-				<h1 className="text-8xl text-white pt-1"> Porfolio </h1>
-				<div className="flex z-10">
-					<img src={backgroundImage} alt="" />
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class Header extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isOpen: false
+		};
+	}
+
+	handleOpen = (e) => {
+		this.setState({ isOpen: !this.state.isOpen });
+	};
+
+	render() {
+		return (
+			<div className="mx-5 flex lg:mx-20 md:mx-10 ">
+				<div className="flex-1 flex">
+					<h1 className="text-6xl text-white pt-1 xl:text-8xl"> Porfolio </h1>
+					<div className="flex z-10">
+						<img src={backgroundImage} className="hidden xl:block" alt="" />
+					</div>
+				</div>
+				<div
+					className="text-white  z-10 flex text-4xl cursor-pointer  items-center md:hidden"
+					onClick={(e) => this.handleOpen(e)}
+				>
+					{this.state.isOpen ? <FaTimes /> : <FaBars />}
+				</div>
+				<div
+					className={`flex flex-2 items-center  ${this.state.isOpen
+						? 'flex flex-col fixed l-0 bg-blue-dark bg-opacity-90 min-h-screen justify-center top-0 w-full'
+						: 'hidden'} md:flex md:items-start md:h-auto md:min-h-0 md:h-full md:w-8/12 md:justify-end md:relative md:flex-row`}
+				>
+					<div className="h-14 z-10 mx-0 border-b-2 justify-end  border-transparent xl:h-28 xl:mx-2 hover:border-red ">
+						<div className="transition duration-300 ease-in-out pt-0 xl:pt-8 hover:bg-red flex items-end">
+							<Link to="#" className="text-white  flex font-bold py-2 px-4 lg:py-4 lg:px-4">
+								About
+							</Link>
+						</div>
+					</div>
+					<div className="h-14 z-10  mx-0 border-b-2  border-transparent xl:h-28 xl:mx-2 hover:border-red ">
+						<div className="transition duration-300 ease-in-out pt-0 xl:pt-8 hover:bg-red flex items-end">
+							<Link to="#" className="text-white  flex font-bold py-2 px-4 lg:py-4 lg:px-4">
+								Portfolio
+							</Link>
+						</div>
+					</div>
+					<div className="h-14 z-10  mx-0 border-b-2  border-transparent xl:h-28 xl:mx-2 hover:border-red">
+						<div className="transition duration-300 ease-in-out pt-0 xl:pt-8 hover:bg-red flex items-end">
+							<Link to="#" className="text-white  flex font-bold py-2 px-4 lg:py-4 lg:px-4">
+								Contact
+							</Link>
+						</div>
+					</div>
+					<div className="h-10 z-10 mx-2 flex items-center mt-1 xl:mt-9 lg:h-16">
+						<Link
+							to="#"
+							className="border bg-transparent border-white rounded-full text-white px-2 py-1 lg:px-4 lg:py-2  hover:text-blue-dark hover:bg-white"
+						>
+							Get started
+						</Link>
+					</div>
 				</div>
 			</div>
-			<div className="flex flex-2 items-start">
-				<div className="h-28 z-10 mx-2 border-b-2  border-transparent hover:border-red">
-					<div className="transition duration-300 ease-in-out hover:bg-red flex items-end pt-8">
-						<Link to="#" className="text-white  flex font-bold py-4 px-8">
-							About
-						</Link>
-					</div>
-				</div>
-				<div className="h-28 z-10  mx-2 border-b-2  border-transparent hover:border-red">
-					<div className="transition duration-300 ease-in-out hover:bg-red flex items-end pt-8">
-						<Link to="#" className="text-white  flex font-bold py-4 px-8">
-							Portfolio
-						</Link>
-					</div>
-				</div>
-				<div className="h-28 z-10  mx-2 border-b-2  border-transparent hover:border-red">
-					<div className="transition duration-300 ease-in-out hover:bg-red flex items-end pt-8">
-						<Link to="#" className="text-white  flex font-bold py-4 px-8">
-							Contact
-						</Link>
-					</div>
-				</div>
-				<div className="h-16 z-10 mx-2 flex items-center mt-9">
-					<Link
-						to="#"
-						className="border bg-transparent border-white rounded-full text-white px-4 py-2  hover:text-blue-dark hover:bg-white"
-					>
-						Get started
-					</Link>
-				</div>
-			</div>
-		</div>
-	);
-};
+		);
+	}
+}
